0563301ae8f52f0faab67741515935f616fb7afe
migrosdata <antoine.induni@gmail.com>
2019-03-27 19:35:27 +0100

    Add persitence servcie

--- src/main/java/ch/hearc/ig/odi/minishop/restresources/CustomerResource.java
+++ src/main/java/ch/hearc/ig/odi/minishop/restresources/CustomerResource.java
@@ -4,6 +4,8 @@
 
 package ch.hearc.ig.odi.minishop.restresources;
 
+import ch.hearc.ig.odi.minishop.services.PersistenceService;
+import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -13,6 +15,7 @@ import javax.ws.rs.core.MediaType;
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 public class CustomerResource {
-
+  @Inject
+  private PersistenceService persistenceService;
 }
 
