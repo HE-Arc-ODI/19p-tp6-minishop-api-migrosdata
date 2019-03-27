001a46a75d7773c94fad7e9d4ba1a86a2b5f133e
migrosdata <antoine.induni@gmail.com>
2019-03-27 19:34:39 +0100

    Add path and media type

--- src/main/java/ch/hearc/ig/odi/minishop/restresources/CustomerResource.java
+++ src/main/java/ch/hearc/ig/odi/minishop/restresources/CustomerResource.java
@@ -4,6 +4,14 @@
 
 package ch.hearc.ig.odi.minishop.restresources;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("customer")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 public class CustomerResource {
 
 }
