af69894868d653134c562b8bd2cee4a2a2e6d2c7
migrosdata <antoine.induni@gmail.com>
2019-03-31 16:04:28 +0200

    Add post and put ressources

--- src/main/java/ch/hearc/ig/odi/minishop/restresources/CustomerResource.java
+++ src/main/java/ch/hearc/ig/odi/minishop/restresources/CustomerResource.java
@@ -7,12 +7,15 @@ package ch.hearc.ig.odi.minishop.restresources;
 import ch.hearc.ig.odi.minishop.business.Customer;
 import ch.hearc.ig.odi.minishop.exception.CustomerException;
 import ch.hearc.ig.odi.minishop.exception.NotFoundException;
-import ch.hearc.ig.odi.minishop.exception.StoreException;
+import ch.hearc.ig.odi.minishop.exception.NullFormException;
 import ch.hearc.ig.odi.minishop.services.PersistenceService;
 import java.util.List;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -39,6 +42,28 @@ public class CustomerResource {
     } catch (CustomerException e) {
       e.printStackTrace();
       throw new NotFoundException("the cart does not exist");
+    }
+  }
+
+  @POST
+  public Customer customerPost(
+      @FormParam("username") String username,
+      @FormParam("firstname") String firstname,
+      @FormParam("lastname") String lastname,
+      @FormParam("email") String email,
+      @FormParam("phone") String phone) {
+    return persistenceService.createAndPersistCustomer(username, firstname, lastname, email, phone);
+  }
+
+  @PUT
+  @Path("{id}")
+  @Consumes(MediaType.APPLICATION_JSON)
+  public Customer updateCustomer(@PathParam("id") Long id, Customer customer) {
+    try {
+      return persistenceService.updateCustomer(id, customer);
+    } catch (CustomerException e) {
+      e.printStackTrace();
+      throw new NullFormException("customer couldn't have been updated.");
     }
   }
 }
